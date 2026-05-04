4eee2003fea02f2c9a963467bcea04f2d941fdb2
mohanabhorkade-cyber <mohanabhorkade@gmail.com>
2026-05-04 15:08:14 +0530

    Fix nodemailer compatibility with Node 24

--- backend/server.js
+++ backend/server.js
@@ -137,7 +137,7 @@ const inquirySchema = Joi.object({
 });
 
 // Configure email transporter
-const transporter = nodemailer.createTransporter({
+const transporter = nodemailer.createTransport({
   service: 'gmail',
   auth: {
     user: process.env.EMAIL_USER,
