0767868041790a751c9d969e0f48a84762778026
mohanabhorkade-cyber <mohanabhorkade@gmail.com>
2026-05-09 23:24:59 +0530

    fix: restore frontend static server

--- server.js
+++ server.js
@@ -1,17 +1,15 @@
-// const express = require('express');
-// const path = require('path');
-// const app = express();
+const express = require('express');
+const path = require('path');
+const app = express();
 
-// // const distPath = path.join(__dirname, 'dist/sparrow-foods/browser');
-// const distPath = path.join(__dirname, 'dist/sparrow-food/browser');
+const distPath = path.join(__dirname, 'dist/sparrow-food/browser');
+app.use(express.static(distPath));
 
-// app.use(express.static(distPath));
+app.get('*', (req, res) => {
+  res.sendFile(path.join(distPath, 'index.html'));
+});
 
-// app.get('*', (req, res) => {
-//   res.sendFile(path.join(distPath, 'index.html'));
-// });
-
-// const PORT = process.env.PORT || 4200;
-// app.listen(PORT, '0.0.0.0', () => {
-//   console.log(`Angular app running on port ${PORT}`);
-// });
+const PORT = process.env.PORT || 4200;
+app.listen(PORT, '0.0.0.0', () => {
+  console.log(`Angular app running on port ${PORT}`);
+});
