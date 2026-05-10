1ac5c447ae5e03254d547321422f58acf931cbfc
mohanabhorkade-cyber <mohanabhorkade@gmail.com>
2026-05-10 12:11:22 +0530

    fix: add sitemap.xml and robots.txt to frontend server

--- server.js
+++ server.js
@@ -5,6 +5,24 @@ const app = express();
 const distPath = path.join(__dirname, 'dist/sparrow-food/browser');
 app.use(express.static(distPath));
 
+// Sitemap for SEO
+app.get('/sitemap.xml', (req, res) => {
+  const baseUrl = `${req.protocol}://${req.get('host')}`;
+  const urls = ['/', '/products', '/contact', '/about'];
+  const sitemapXml = `<?xml version="1.0" encoding="UTF-8"?>
+<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
+${urls.map(url => `  <url><loc>${baseUrl}${url}</loc><changefreq>weekly</changefreq><priority>0.8</priority></url>`).join('\n')}
+</urlset>`;
+  res.type('application/xml');
+  res.send(sitemapXml);
+});
+
+// Robots.txt
+app.get('/robots.txt', (req, res) => {
+  res.type('text/plain');
+  res.send(`User-agent: *\nAllow: /\nSitemap: ${req.protocol}://${req.get('host')}/sitemap.xml\n`);
+});
+
 app.get('*', (req, res) => {
   res.sendFile(path.join(distPath, 'index.html'));
 });
